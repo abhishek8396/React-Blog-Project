8ea6e2b8bea789c5fbbb767af37b2ea1a76bf5c6
anhishek1289@gmail.com <abhishek1289@gmail.com>
2023-02-02 22:11:57 +0530

    react

---
+++ src/Pages/bollywood/Bollywood.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReusableComponent from '../ReusableComponent'
+
+
+
+function Bollywood() {
+  const bollywoodTiles =[
+    {
+      img:'https://i.gadgets360cdn.com/products/large/Chhatriwali-1-800x800-1674207243685.jpg?downsize=*:420',
+      title:'Chhatriwali',
+      description:'A condom factorys latest quality control head is a shy woman (Rakul Preet Singh) who is embarrassed about her job. Eventually, she realises the significance of safe sex, and decides to take it upon herself to destigmatise the conversation around sex education.',
+      releaseDate:'20 January 2023'
+    },
+    {
+      img:'https://i.gadgets360cdn.com/products/large/India-Lockdown-1-640x800-1667906603509.jpg?downsize=*:420',
+      title:'India Lockdown',
+      description:'Four parallel stories exploring the impact of the COVID-19 pandemic on the people of India, inspired by true events.The first story explores a father-daughter duo stuck in different states in the crisis, when the latter is at a very crucial point of her life.A sex workers Shweta Basu Prasad) share of personal and professional challenges during the period follows next.',
+      releaseDate:'2 December 2022',
+    },
+    {
+      img:'https://i.gadgets360cdn.com/products/large/drishyam-2-3-1422x800-1668080885271.jpg?downsize=*:420',
+      title:'Drishyam 2',
+      description:'Trouble has once again surrounded the Salgaonkars; now that new evidence has entered the picture.Like last time, Vijay Salgaonkar (Ajay Devgn) is determined to protect his family and prevent them from going to prison — even if it means crossing new limits.The crime thriller serves as a sequel to Nisikant Kamats Drishyam (2015), and is the official remake of the 2021 Malayalam suspense drama of the same name.',
+      releaseDate:'18 November 2022',
+    },
+    
+  ]
+  return (
+    <div className='home-bollywood'>
+      <div className='bollywood-titles'>
+        <span className='latest-bollywood'>Latest Bollywood News</span>
+      </div>
+      <div className='bollywoodTiles'>
+            <ReusableComponent props={bollywoodTiles}/>
+      </div>
+
+    </div>
+  )
+}
+
+export default Bollywood
