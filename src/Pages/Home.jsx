e7f766ba6804ba5cd5dd275f6482ddb327577f62
anhishek1289@gmail.com <abhishek1289@gmail.com>
2023-02-06 19:02:06 +0530

    blog

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,11 +17,11 @@ function Home() {
     <div className='main-box'>
       <div className='home-container'>
         <div className='my-img'>
-      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1280px-24701-nature-natural-beauty.jpg' className='first'></img>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1280px-24701-nature-natural-beauty.jpg' alt='empty' className='first'></img>
       </div>
       <div className='small-pic'>
-      <img src='https://www.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg' className='second'></img>
-      <img src='https://i.insider.com/5e83756d487c225d3b6e1c86?width=1000&format=jpeg&auto=webp' className='third'></img>
+      <img src='https://www.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg' alt='empty' className='second'></img>
+      <img src='https://i.insider.com/5e83756d487c225d3b6e1c86?width=1000&format=jpeg&auto=webp' alt='empty' className='third'></img>
       </div>
       
       
